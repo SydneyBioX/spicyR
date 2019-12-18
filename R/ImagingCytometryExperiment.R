220cbf6e322796d2157d92252fedd5a515c74d1c
Ellis Patrick <epatrick87@gmail.com>
2019-12-19 06:41:05 +1100

    Added ImagingCytometryExperiment

---
+++ R/ImagingCytometryExperiment.R
@@ -0,0 +1,153 @@
+### Convert something into a ImagingCytometryExperiment
+
+as.ImagingCytometryExperiment <- function(x){
+  if(any(grepl('Intensity_mean',colnames(cells)))){
+    
+    df = DataFrame(row.names = unique(x$imageID))
+    location <- split(DataFrame(x[,c('cellID','cellType','x','y')]), x$imageID)
+    df$location <- location
+    intensity <- x[,grep('Intensity_mean',colnames(x))]
+    colnames(intensity) <- gsub('Intensity_mean_', '', intensity)
+    df$intensity = split(DataFrame(intensity), x$imageID)
+    morphology <- x[,grep('Area',colnames(x))]
+    colnames(morphology) <- gsub('Area_', '', morphology)
+    df$morphology = split(DataFrame(morphology), x$imageID)
+    df$clinical = split(DataFrame(), x$imageID)
+    return(df)
+  }
+}
+
+
+### Some functions to get and make location data
+
+location <- function(x, image = NULL, bind = FALSE){
+  if(!is.null(image)){
+    x = x[image,]
+  }
+  if(bind==FALSE){
+    return(x$location)
+  }
+  if(bind==TRUE){
+    return(do.call('rbind',x$location))
+  }
+}
+
+
+'location<-' <- function(x, value, bind=FALSE){
+  if(nrow(value)==nrow(x)){
+  x$location <- value
+  return(x)
+  }
+  
+  if(nrow(value)==length(imageID(x,bind ==TRUE))){
+    x$location <- split(value,rep(rownames(x),unlist(lapply(x$location,nrow))))
+    return(x)
+  }
+    
+}
+
+
+### Get imageIDs for each cell, not sure if this should also report rownames(df)
+
+imageID <- function(x, image = NULL, bind = FALSE){
+  if(!is.null(image)){
+    x = x[image,]
+  }
+  rep(rownames(x),unlist(lapply(x$location,nrow)))
+}
+
+### Get cellTypes for each cell
+
+cellType <- function(x, image = NULL){
+  if(!is.null(image)){
+    x = x[image,]
+  }
+  do.call('rbind',x$location)$cellType
+  }
+
+
+cellID <- function(x, image = NULL){
+  if(!is.null(image)){
+    x = x[image,]
+  }
+  do.call('rbind',x$location)$cellID
+}
+
+
+
+#### I can access and add clinical data to the object
+
+clinical <- function(x, image = NULL, bind = FALSE){
+  if(!is.null(image)){
+    x = x[image,]
+  }
+    do.call('rbind',x$clinical)
+  }
+
+
+
+'clinical<-' <- function(x, value, image = NULL){
+  if(is.null(image)) image <- rownames(x)
+  use <- intersect(value$imageID,image)
+  x <- x[image,] 
+  x$clinical <- split(value,image)
+  x
+}
+
+
+
+
+
+# library(S4Vectors)
+# 
+# 
+# 
+# ### Something that resembles cellProfiler data
+# 
+# cells <- data.frame(row.names = 1:10)
+# cells$cellID <- paste('cell',1:10,sep = '')
+# cells$imageID <- paste('image',rep(1:2,c(4,6)),sep = '')
+# cells$x <- 1:10
+# cells$y <- 10:1
+# cells$cellType <- paste('cellType',rep(1:2,5),sep = '')
+# cells$Area_round <- 1
+# cells$Area_diameter <- 2
+# cells$Intensity_mean_CD8 <- 3
+# cells$Intensity_mean_CD4 <- 11:20
+# 
+# 
+# 
+# ### Lets make a CellImagingExperiment... obviously we can make this a class
+# 
+# df <- as.ImagingCytometryExperiment(cells)
+# 
+# 
+# 
+# clinicalData <- DataFrame(sex = c('M',"F"), height = c(120,210), weight = c(30, 140), imageID = c('image1','image2'))
+# clinical(df) <- clinicalData
+# 
+# ### I can get the locations and set them again
+# z <- location(df)
+# z
+# 
+# ### If I give it a list of DataFrames
+# location(df) <- z
+# 
+# 
+# z <- location(df, bind = TRUE)
+# z
+# ### If I give it a single DataFrame
+# location(df) <- z
+# 
+# 
+# ### I can pull out information for specific images
+# df['image1',]
+# location(df['image1',])
+# location(df,'image1')
+# location(df,c('image1','image2'),TRUE)
+# 
+# cellType(df)
+# 
+# 
+# 
+# ### We could also add an image(df) which contained stacks maybe even image(df,'image1', 'CD8')
--- R/geomHatching.R
+++ R/geomHatching.R
@@ -1,183 +1,153 @@
-# scale_region <- function(..., guide = "legend") {
-#   sc <- discrete_scale("region", "identity", scales::identity_pal(), super = ScaleDiscreteIdentity, ..., guide = guide)
-#   sc
-# }
-
-
 scale_region <- function(aesthetics = "region", ..., guide = "legend") {
-discrete_scale("region", "region_d", palette = function(n) 1:n, ...)
+    discrete_scale("region", "region_d", palette = function(n) 1:n, ...)
 }
 
-scale_region_manual <- function (..., values){
-
-
+scale_region_manual <- function(..., values) {
+    
+    
     force(values)
     pal <- function(n) {
-      if (n > length(values)) {
-        stop("Insufficient values in manual scale. ",
-             n, " needed but only ", length(values),
-             " provided.", call. = FALSE)
-      }
-      if (any(!values%in%1:7)) {
-        stop("values must be between 1 and 7")
-      }
-
-      values
+        if (n > length(values)) {
+            stop("Insufficient values in manual scale. ", n, " needed but only ", 
+                length(values), " provided.", call. = FALSE)
+        }
+        if (any(!values %in% 1:7)) {
+            stop("values must be between 1 and 7")
+        }
+        
+        values
     }
     discrete_scale("region", "manual", pal, ...)
-
+    
 }
 
 
 ggname <- getFromNamespace("ggname", "ggplot2")
 
-draw_key_region <- function (data, params, size) {
-  cat(names(data),'\n')
-  cat(dim(data),'\n')
-  cat(unlist(data),'\n')
-  cat(names(params),'\n')
-  cat(unlist(params),'\n')
-  cat(size,'\n')
-  #cat(hatching,'\n')
-
-  cat(data$region,'\n')
-  # if(is.null(hatching)){ hatching = 1:7
-  # names(hatching) = paste('r',1:7,sep='')}
-
-  grobs = grob()
-
-  # if(is.character(data$region)){
-  #    grobs =   polylineGrob(x=c(0,0,1,1,0),
-  #                          y= c(0,1,1,0,0),
-  #                          id=c(1,1,1,1,1),
-  #                          gp=gpar(col=0, lwd=1))
-  #
-
-
-  if(data$region==1) {grobs =   polylineGrob(x=c(0,0,1,1,0),
-                                                       y= c(0,1,1,0,0),
-                                                       id=c(1,1,1,1,1),
-                                                       gp=gpar(col=1, lwd=1))}
-
-  if(data$region==2) {grobs =   polylineGrob(x=c(c(0,0.5),c(0,1),c(0.5,1), c(0,0,1,1,0)),
-                                                       y=c(c(0.5,1),c(0,1),c(0,0.5), c(0,1,1,0,0)),
-                                                       id=c(1,1,2,2,3,3,4,4,4,4,4),
-                                                       gp=gpar(col=1, lwd=1))}
-  if(data$region==3){
-    grobs = polylineGrob(x=c(c(1,0.5),c(1,0),c(0.5,0), c(0,0,1,1,0)),
-                         y=c(c(0.5,1),c(0,1),c(0,0.5), c(0,1,1,0,0)),
-                         id=c(1,1,2,2,3,3,4,4,4,4,4),
-                         gp=gpar(col=1, lwd=1))
-
-  }
-
-  if(data$region==4){
-    grobs = polylineGrob(x=c(c(0,1),c(0,1), c(0,0,1,1,0)),
-                         y=c(c(0.33,0.33),c(0.66,0.66), c(0,1,1,0,0)),
-                         id=c(1,1,2,2,3,3,3,3,3),
-                         gp=gpar(col=1, lwd=1))
-
-  }
-
-  if(data$region==5){
-    grobs = polylineGrob(x=c(c(0.33,0.33),c(0.66,0.66), c(0,0,1,1,0)),
-                         y=c(c(0,1),c(0,1), c(0,1,1,0,0)),
-                         id=c(1,1,2,2,3,3,3,3,3),
-                         gp=gpar(col=1, lwd=1))
-
-  }
-
-
-  if(data$region==6) {grobs =   polylineGrob(x=c(c(1,0.5),c(1,0),c(0.5,0),c(0,0.5),c(0,1),c(0.5,1), c(0,0,1,1,0)),
-                                                       y=c(c(0.5,1),c(0,1),c(0,0.5),c(0.5,1),c(0,1),c(0,0.5), c(0,1,1,0,0)),
-                                                       id=c(1,1,2,2,3,3,4,4,5,5,6,6,7,7,7,7,7),
-                                                       gp=gpar(col=1, lwd=1))}
-
-  if(data$region==7){
-    grobs = polylineGrob(x=c(c(0,1),c(0,1),c(0.33,0.33),c(0.66,0.66), c(0,0,1,1,0)),
-                         y=c(c(0.33,0.33),c(0.66,0.66),c(0,1),c(0,1), c(0,1,1,0,0)),
-                         id=c(1,1,2,2,3,3,4,4,5,5,5,5,5),
-                         gp=gpar(col=1, lwd=1))
-
-  }
-  grobs$name = 'region_key'
-  grobs
+draw_key_region <- function(data, params, size) {
+    grobs <- grob()
+    
+    
+    if (data$region == 1) {
+        grobs <- polylineGrob(x = c(0, 0, 1, 1, 0), y = c(0, 1, 1, 0, 0), id = c(1, 
+            1, 1, 1, 1), gp = gpar(col = 1, lwd = 1))
+    }
+    
+    if (data$region == 2) {
+        grobs <- polylineGrob(x = c(c(0, 0.5), c(0, 1), c(0.5, 1), c(0, 0, 1, 1, 
+            0)), y = c(c(0.5, 1), c(0, 1), c(0, 0.5), c(0, 1, 1, 0, 0)), id = c(1, 
+            1, 2, 2, 3, 3, 4, 4, 4, 4, 4), gp = gpar(col = 1, lwd = 1))
+    }
+    if (data$region == 3) {
+        grobs <- polylineGrob(x = c(c(1, 0.5), c(1, 0), c(0.5, 0), c(0, 0, 1, 1, 
+            0)), y = c(c(0.5, 1), c(0, 1), c(0, 0.5), c(0, 1, 1, 0, 0)), id = c(1, 
+            1, 2, 2, 3, 3, 4, 4, 4, 4, 4), gp = gpar(col = 1, lwd = 1))
+        
+    }
+    
+    if (data$region == 4) {
+        grobs <- polylineGrob(x = c(c(0, 1), c(0, 1), c(0, 0, 1, 1, 0)), y = c(c(0.33, 
+            0.33), c(0.66, 0.66), c(0, 1, 1, 0, 0)), id = c(1, 1, 2, 2, 3, 3, 3, 
+            3, 3), gp = gpar(col = 1, lwd = 1))
+        
+    }
+    
+    if (data$region == 5) {
+        grobs <- polylineGrob(x = c(c(0.33, 0.33), c(0.66, 0.66), c(0, 0, 1, 1, 0)), 
+            y = c(c(0, 1), c(0, 1), c(0, 1, 1, 0, 0)), id = c(1, 1, 2, 2, 3, 3, 3, 
+                3, 3), gp = gpar(col = 1, lwd = 1))
+        
+    }
+    
+    
+    if (data$region == 6) {
+        grobs <- polylineGrob(x = c(c(1, 0.5), c(1, 0), c(0.5, 0), c(0, 0.5), c(0, 
+            1), c(0.5, 1), c(0, 0, 1, 1, 0)), y = c(c(0.5, 1), c(0, 1), c(0, 0.5), 
+            c(0.5, 1), c(0, 1), c(0, 0.5), c(0, 1, 1, 0, 0)), id = c(1, 1, 2, 2, 
+            3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 7, 7, 7), gp = gpar(col = 1, lwd = 1))
+    }
+    
+    if (data$region == 7) {
+        grobs <- polylineGrob(x = c(c(0, 1), c(0, 1), c(0.33, 0.33), c(0.66, 0.66), 
+            c(0, 0, 1, 1, 0)), y = c(c(0.33, 0.33), c(0.66, 0.66), c(0, 1), c(0, 
+            1), c(0, 1, 1, 0, 0)), id = c(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 5, 5, 5), 
+            gp = gpar(col = 1, lwd = 1))
+        
+    }
+    grobs$name <- "region_key"
+    grobs
+    
+    
+}
 
 
+
+hatchingLevels <- function(data, hatching = NULL) {
+    
+    if (!is.factor(data$region)) 
+        data$region <- factor(data$region)
+    regionLevels <- levels(data$region)
+    if (!any(hatching %in% 1:7) & !is.null(hatching)) 
+        stop("hatching must equal the number of regions and be <= 7.")
+    if (all(regionLevels %in% names(hatching))) {
+        hatching <- hatching[regionLevels]
+    }
+    if (is.null(hatching)) {
+        hatching <- 1:length(regionLevels)
+        names(hatching) <- regionLevels
+    }
+    if (length(hatching) == length(regionLevels)) {
+        names(hatching) <- regionLevels
+    } else {
+        stop("hatching must be the same length as the number of regions.")
+    }
+    return(hatching)
 }
 
 
 
 
-
-
-hatchingLevels <- function(data,hatching = NULL){
-
-  if(!is.factor(data$region))data$region <- factor(data$region)
-  regionLevels = levels(data$region)
-  if(!any(hatching%in%1:7)&!is.null(hatching)) stop('hatching must equal the number of regions and be <= 7.')
-  if(all(regionLevels%in%names(hatching))){
-    hatching <- hatching[regionLevels]
-  }
-  if(is.null(hatching)){
-    hatching = 1:length(regionLevels)
-    names(hatching) = regionLevels
-  }
-  if(length(hatching)==length(regionLevels)){names(hatching) = regionLevels}else{stop('hatching must be the same length as the number of regions.')}
-  return(hatching)
-}
-
-
-
-
-GeomHatching <- ggproto(
-  "GeomHatching",
-  GeomPoint,
-  extra_params = c("na.rm", "line.spacing",  "window", "nbp", "line.width"),
-   draw_panel = function(data, panel_params,
-                        coord, na.rm = FALSE, line.spacing = 21, window = FALSE, nbp = 250, line.width = 1) {
+GeomHatching <- ggproto("GeomHatching", GeomPoint, extra_params = c("na.rm", "line.spacing", 
+    "window", "nbp", "line.width"), draw_panel = function(data, panel_params, coord, 
+    na.rm = FALSE, line.spacing = 21, window = FALSE, nbp = 250, line.width = 1) {
     coords <- coord$transform(data, panel_params)
-
-    if(is.factor(coords$region))coords$region <- as.numeric(coords$region)
-    if(is.character(coords$region))coords$region <- as.numeric(as.factor(coords$region))
-
+    
+    if (is.factor(coords$region)) 
+        coords$region <- as.numeric(coords$region)
+    if (is.character(coords$region)) 
+        coords$region <- as.numeric(as.factor(coords$region))
+    
     ow <- owin(xrange = range(coords$x), yrange = range(coords$y))
-    if(window=='convex'){
-      ch = chull(coords[,c('x','y')])
-      poly <- coords[,c('x','y')][rev(ch),]
-      colnames(poly) = c('x','y')
-      ow <- owin(xrange = range(coords$x), yrange = range(coords$y), poly = poly)
+    if (window == "convex") {
+        ch <- chull(coords[, c("x", "y")])
+        poly <- coords[, c("x", "y")][rev(ch), ]
+        colnames(poly) <- c("x", "y")
+        ow <- owin(xrange = range(coords$x), yrange = range(coords$y), poly = poly)
     }
-    if(window=='concave'){
-      ch = concaveman(as.matrix(coords[,c('x','y')]),length = 0.01)
-      poly <- as.data.frame(ch[nrow(ch):1,])
-      colnames(poly) = c('x','y')
-      ow <- owin(xrange = range(poly$x)+c(-0.1,0.1), yrange = range(poly$y), poly = poly)
+    if (window == "concave") {
+        ch <- concaveman(as.matrix(coords[, c("x", "y")]), length = 0.01)
+        poly <- as.data.frame(ch[nrow(ch):1, ])
+        colnames(poly) <- c("x", "y")
+        ow <- owin(xrange = range(poly$x) + c(-0.1, 0.1), yrange = range(poly$y), 
+            poly = poly)
     }
-       pp <- ppp(coords$x,coords$y,window = ow, marks = coords$region)
-
-    cat('head region', head(data$region), '\n')
-    cat('unique region', unique(data$region), '\n')
+    pp <- ppp(coords$x, coords$y, window = ow, marks = coords$region)
+    
     pp$region <- pp$marks
-    grob <- plotRegions(pp, line.spacing, c(0,1), c(0,1), nbp = nbp, line.width = line.width)
-    grob$name  = "geom_hatching"
+    grob <- plotRegions(pp, line.spacing, c(0, 1), c(0, 1), nbp = nbp, line.width = line.width)
+    grob$name <- "geom_hatching"
     return(grob)
-  },
-  draw_key =  draw_key_region,
-  required_aes = c("x", "y", "region"),
-  non_missing_aes = c("x", "y", "region"),
-  default_aes = aes(region = 0, size=0.05, angle = 0, alpha=1)
-)
+}, draw_key = draw_key_region, required_aes = c("x", "y", "region"), non_missing_aes = c("x", 
+    "y", "region"), default_aes = aes(region = 0, size = 0.05, angle = 0, alpha = 1))
 
 
-geom_hatching <- function(mapping = NULL, data = NULL, stat = "identity",
-                          position = "identity", na.rm = FALSE, show.legend = NA,
-                          inherit.aes = TRUE, line.spacing=21, window = FALSE, nbp =250, line.width=1,...) {
-
-   layer(
-    geom = GeomHatching, mapping = mapping,  data = data, stat = stat,
-    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-    params = list(na.rm = na.rm, line.spacing=line.spacing, window = window, nbp = nbp, line.width = line.width, ...)
-  )
+geom_hatching <- function(mapping = NULL, data = NULL, stat = "identity", position = "identity", 
+    na.rm = FALSE, show.legend = NA, inherit.aes = TRUE, line.spacing = 21, window = FALSE, 
+    nbp = 250, line.width = 1, ...) {
+    
+    layer(geom = GeomHatching, mapping = mapping, data = data, stat = stat, position = position, 
+        show.legend = show.legend, inherit.aes = inherit.aes, params = list(na.rm = na.rm, 
+            line.spacing = line.spacing, window = window, nbp = nbp, line.width = line.width, 
+            ...))
 }
 
