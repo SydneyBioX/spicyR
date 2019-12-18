18a11c2443fb2db7b80d2d22c2280e41f68c982f
Nicolas Canete <nickcee900@gmail.com>
2019-12-18 15:50:30 +1100

    Commit Lplot and Lbootstrap functions

---
+++ R/Lbootstrap.R
@@ -0,0 +1,35 @@
+Lbootstrap <- function(cells, 
+                       from, 
+                       to, 
+                       rinterval=c(0,100),
+                       nsim = 199,
+                       global = FALSE,
+                       alternative=c("two.sided", "less", "greater"),
+                       seed = 42) {
+  
+    #TODO: Produce error messages
+    # e.g. Missing entries in 'cells', 'from' or 'to' not present in cellTypes
+
+    pppCell <- ppp(cells$x,
+                   cells$y,
+                   xrange = c(0,max(x)),
+                   ytange = c(0,max(y)),
+                   marks = cells$cellType)
+    
+    set.seed(seed)
+    
+    E = spatstat::envelope(pppCell,
+                           Lcross.inhom,
+                           nsim = nsim,
+                           i = 'Tumour',
+                           j = 'Tumour',
+                           simulate = expression(rlabel(pppCell)),
+                           global = global, 
+                           savepatterns = TRUE)
+    
+    test = spatstat::dclf.test(pppCell,
+                               rinterval = rinterval,
+                               alternative = alternative)
+    
+    return(test)
+}
---
+++ R/Lplot.R
@@ -0,0 +1,13 @@
+Lplot <- function(cells, from, to, rinterval=c(0,100)) {
+    #TODO: Produce error messages
+    # e.g. Missing entries in 'cells', 'from' or 'to' not present in cellTypes
+    #TODO: make plot with ggplot
+    
+    pppCell <- spatstat::ppp(cells$x,
+                             cells$y,
+                             xrange = c(0,max(x)),
+                             yrange = c(0,max(y)),
+                            marks = cells$cellType)
+    
+    plot(spatstat::Lcross(pppCell, from=from, to=to), xrange=rinterval)
+}
