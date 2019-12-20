2f5f9267c543e7787fecf83e086db285b2a22d75
Ellis Patrick <epatrick87@gmail.com>
2019-12-20 12:28:59 +1100

    commit for Nick

--- R/SpatialCellExperiment.R
+++ R/SpatialCellExperiment.R
@@ -1,8 +1,7 @@
 ### Convert something into a ImagingCytometryExperiment
 
-as.ImagingCytometryExperiment <- function(x){
+as.SpatialCellExperiment <- function(x){
   if(any(grepl('Intensity_mean',colnames(cells)))){
-    
     df = DataFrame(row.names = unique(x$imageID))
     location <- split(DataFrame(x[,c('cellID','cellType','x','y')]), x$imageID)
     df$location <- location
@@ -12,7 +11,9 @@ as.ImagingCytometryExperiment <- function(x){
     morphology <- x[,grep('Area',colnames(x))]
     colnames(morphology) <- gsub('Area_', '', morphology)
     df$morphology = split(DataFrame(morphology), x$imageID)
-    df$clinical = split(DataFrame(), x$imageID)
+    df$phenotype = split(DataFrame(), x$imageID)
+    df$images = split(DataFrame(), x$imageID)
+    df$masks = split(DataFrame(), x$imageID)
     return(df)
   }
 }
@@ -126,22 +127,22 @@ makeUniqueCellID <- function(x){
 
 
 
-#### I can access and add clinical data to the object
+#### I can access and add phenotype data to the object
 
-clinical <- function(x, image = NULL, bind = FALSE){
+phenotype <- function(x, image = NULL, bind = FALSE){
   if(!is.null(image)){
     x = x[image,]
   }
-    do.call('rbind',x$clinical)
+    do.call('rbind',x$phenotype)
   }
 
 
 
-'clinical<-' <- function(x, value, image = NULL){
+'phenotype<-' <- function(x, value, image = NULL){
   if(is.null(image)) image <- rownames(x)
   use <- intersect(value$imageID,image)
   x <- x[image,] 
-  x$clinical <- split(value,image)
+  x$phenotype <- split(value,image)
   x
 }
 
@@ -153,29 +154,29 @@ clinical <- function(x, image = NULL, bind = FALSE){
 # 
 # 
 # 
-# ### Something that resembles cellProfiler data
+### Something that resembles cellProfiler data
+
+cells <- data.frame(row.names = 1:10)
+cells$cellID <- paste('cell',1:10,sep = '')
+cells$imageID <- paste('image',rep(1:2,c(4,6)),sep = '')
+cells$x <- 1:10
+cells$y <- 10:1
+cells$cellType <- paste('cellType',rep(1:2,5),sep = '')
+cells$Area_round <- 1
+cells$Area_diameter <- 2
+cells$Intensity_mean_CD8 <- 3
+cells$Intensity_mean_CD4 <- 11:20
+
+
+
+### Lets make a CellImagingExperiment... obviously we can make this a class
+
+df <- as.ImagingCytometryExperiment(cells)
 # 
-# cells <- data.frame(row.names = 1:10)
-# cells$cellID <- paste('cell',1:10,sep = '')
-# cells$imageID <- paste('image',rep(1:2,c(4,6)),sep = '')
-# cells$x <- 1:10
-# cells$y <- 10:1
-# cells$cellType <- paste('cellType',rep(1:2,5),sep = '')
-# cells$Area_round <- 1
-# cells$Area_diameter <- 2
-# cells$Intensity_mean_CD8 <- 3
-# cells$Intensity_mean_CD4 <- 11:20
 # 
 # 
-# 
-# ### Lets make a CellImagingExperiment... obviously we can make this a class
-# 
-# df <- as.ImagingCytometryExperiment(cells)
-# 
-# 
-# 
-# clinicalData <- DataFrame(sex = c('M',"F"), height = c(120,210), weight = c(30, 140), imageID = c('image1','image2'))
-# clinical(df) <- clinicalData
+# phenotypeData <- DataFrame(sex = c('M',"F"), height = c(120,210), weight = c(30, 140), imageID = c('image1','image2'))
+# phenotype(df) <- phenotypeData
 # 
 # ### I can get the locations and set them again
 # z <- location(df)
