7df620de5c0fb261fbf86f464de590c4083f4494
Ellis Patrick <epatrick87@gmail.com>
2020-07-21 22:07:25 +1000

    changed statistic into t value

--- R/spicy.R
+++ R/spicy.R
@@ -318,13 +318,13 @@ spatialMEMBootstrap <- function(mixed.lmer, nsim = 19) {
         
         
         
-        summary(mixed.lmer1)$coef[2, "statistic"]
+        summary(mixed.lmer1)$coef[2, "t value"]
     }
     
     stats <- replicate(nsim, functionToReplicate(x = mixed.lmer))
     stats <- t(stats)
     fe <- fixef(mixed.lmer)
-    pval <- mean(abs(stat)>abs(summary(mixed.lmer)$coef[2, "statistic"]))
+    pval <- mean(abs(stat)>abs(summary(mixed.lmer)$coef[2, "t value"]))
     df <-
         data.frame(
             coefficient = fe,
