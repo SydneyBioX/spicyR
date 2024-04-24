5f387c43dcab4588beabfa5b373d7681f0a6efe7
Nick <nickrrrr@gmail.com>
2024-04-24 11:30:22 +1000

    use orginal_data in tests

--- tests/testthat/test-spicyR.R
+++ tests/testthat/test-spicyR.R
@@ -1,16 +1,13 @@
 test_that(
     "the output is equal using segmented cells and SingleCellExperiment",
     {
+        load(system.file("testdata/original_result.rda", package = "spicyR"))
         expect_equal(
             suppressWarnings(spicy(diabetesData_SCE,
-                imageID = "imageID",
                 condition = "stage", subject = "case",
                 from = "Tc", to = "Th"
             )),
-            suppressWarnings(spicy(diabetesData,
-                condition = "stage", subject = "case",
-                from = "Tc", to = "Th"
-            ))
+            original_result
         )
     }
 )
