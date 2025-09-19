fe47ce5ee644c542057b5e5163cfc30865fd8f62
ellispatrick <ellis.patrick@sydney.edu.au>
2025-09-19 20:34:12 +1000

    linewidth

--- R/signifPlot.R
+++ R/signifPlot.R
@@ -543,7 +543,7 @@ survBubble = function(result,
       ggplot2::guides(fill = guide_legend(order = 2, override.aes = list(alpha = 1))) +
       theme(strip.text = element_text(size = -1),
             strip.clip = "off",
-            strip.background = element_rect(linewidth = NA),
+            strip.background = element_rect(linewidth = 0),
             panel.spacing = unit(0.4,'lines'))
   }
   
